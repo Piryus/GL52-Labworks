bdf1ff24516554a42547f4b9e46c5fcbbcbba6ff
Emile <legendre.emile.27@gmail.com>
2019-04-01 21:01:19 +0200

    Add @Override keyword to functions in lab #1

--- src/re/legend/utbm/gl52/lab1/AbstractStack.java
+++ src/re/legend/utbm/gl52/lab1/AbstractStack.java
@@ -3,10 +3,12 @@ package re.legend.utbm.gl52.lab1;
 abstract class AbstractStack<ElementT> implements IStack<ElementT> {
     protected int size;
 
+    @Override
     public int getSize() {
         return size;
     }
 
+    @Override
     public boolean isEmpty() {
         return size == 0;
     }
