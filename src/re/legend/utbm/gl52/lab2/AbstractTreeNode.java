cc5a518c4003ebea1ebe960642cb88b91a6f14f1
Emile <legendre.emile.27@gmail.com>
2019-04-07 22:51:54 +0200

    Add Javadoc in AbstractTreeNode

--- src/re/legend/utbm/gl52/lab2/AbstractTreeNode.java
+++ src/re/legend/utbm/gl52/lab2/AbstractTreeNode.java
@@ -1,15 +1,13 @@
 package re.legend.utbm.gl52.lab2;
 
 /**
- * 
+ * This class is the highest abstraction for a tree node
+ * It contains the data member and the methods it is linked to
  */
 public abstract class AbstractTreeNode<D, N extends AbstractTreeNode<D,N>> implements ITreeNode<D,N> {
 
-    /**
-     * the data contained in the node
-     */
+    // The data contained in the node
     protected D data;
-
 
     @Override
     public D getData() {
